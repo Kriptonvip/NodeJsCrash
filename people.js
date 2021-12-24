eeb9bb213807612c635cc1d5a77f096ec11d365c
Kripton <simenk@mail.ru>
2021-12-24 21:29:04 +0300

    first lesson Node Crash Tutorial, fs module, require, exports.

---
+++ people.js
@@ -0,0 +1,10 @@
+const people =['yoshi','ruy','chun-li','mario'];
+
+const ages = [20, 30,20, 50];
+console.log(people);
+
+
+module.exports = {
+    people: people,
+    ages:ages
+};
---
+++ test.js
@@ -0,0 +1,8 @@
+const name1 = "yoshi";
+
+//console.log(name1);
+
+const great = (name1) => {
+    console.log(`hello, ${name1}`);
+}
+great('Yoshi');
